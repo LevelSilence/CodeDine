040aa7e07c0f14519b00ec5fecbe5b5d6cde5a55
Yuvaraj <iec2024051@iiita.ac.in>
2025-08-20 01:54:40 +0530

    fixed the issue finally

--- frontend/src/services/api.js
+++
@@ -1,6 +0,0 @@
-export async function fetchCategories(params) {
-  const query = new URLSearchParams(params).toString();
-  const res = await fetch(`${import.meta.env.VITE_API_BASE}/api/v1/content?${query}`);
-  if (!res.ok) throw new Error('Failed to fetch categories');
-  return res.json();
-}
