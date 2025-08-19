27cc72f83d474f19f80874ef6bad1adaa65b2081
Yuvaraj <iec2024051@iiita.ac.in>
2025-08-20 01:07:09 +0530

    trying to fix a issue which fails backend integration

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:5173','https://code-dine.vercel.app'],
+  origin: ['http://localhost:5173','https://codedine.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
